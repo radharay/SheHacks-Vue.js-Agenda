43739a0a01741d3cc0367c5987b1bc1dfd9f63e8
radharay <radharayg@gmail.com>
2019-09-15 11:50:08 -0300

    Incluindo CSS no Componente

--- calculadora-app/src/components/Calculadora.vue
+++ calculadora-app/src/components/Calculadora.vue
@@ -1,113 +1,57 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links, teste</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div class="calculadora">
+    <div class="display">0</div>
+    <div class="botao">C</div>
+    <div class="botao">+/-</div>
+    <div class="botao">%</div>
+    <div class="botao operadores">÷</div>
+    <div class="botao">7</div>
+    <div class="botao">8</div>
+    <div class="botao">9</div>
+    <div class="botao operadores">x</div>
+    <div class="botao">4</div>
+    <div class="botao">5</div>
+    <div class="botao">6</div>
+    <div class="botao operadores">-</div>
+    <div class="botao">1</div>
+    <div class="botao">2</div>
+    <div class="botao">3</div>
+    <div class="botao operadores">+</div>
+    <div class="botao zero">0</div>
+    <div class="botao">.</div>
+    <div class="botao">=</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data() {
-    return {
-      msg: 'Welcome to Your Vue.js App',
-    };
-  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.calculadora {
+  margin: 0 auto;
+  width: 350px;
+  font-size: 40px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto);
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.display {
+  grid-column: 1 / 5;
+  background-color: #333;
+  color: white;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.zero {
+  grid-column: 1 / 3;
 }
-a {
-  color: #42b983;
+.botao {
+  background-color: #f2f2f2;
+  border: 1px solid #999;
+}
+.operadores {
+  background-color: orange;
+  color: white;
 }
 </style>
